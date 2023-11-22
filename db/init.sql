6c66f99ee50014802e2545da01e813f937b21062
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-11-22 18:46:38 +0000

    update sql

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "dbo.sensor_data" (
+CREATE TABLE "sensor_data" (
 	"id" uniqueidentifier PRIMARY KEY NOT NULL,
 	"sensor_id" integer NOT NULL,
 	"temperature" integer NOT NULL,
 	"wind_speed" integer NOT NULL,
 	"relative_humidity" integer NOT NULL,
 	"co2" integer NOT NULL,
-	"recorded_at" timestamp NOT NULL,
+	"recorded_at" datetime2 NOT NULL,
 	CONSTRAINT "unique_recorded_at" UNIQUE("id","recorded_at")
 );
