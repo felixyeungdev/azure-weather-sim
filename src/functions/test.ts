e2800b43679e829d94a61eb6a1ab25c7b57a20d6
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2023-11-22 19:52:21 +0000

    runs on timer instead of http

--- src/functions/test.ts
+++ src/functions/test.ts
@@ -1,12 +1,5 @@
-import {
-  app,
-  HttpRequest,
-  HttpResponseInit,
-  InvocationContext,
-  output,
-} from "@azure/functions";
+import { app, InvocationContext, output, Timer } from "@azure/functions";
 import { sensors } from "../sensors/sensors";
-import { json } from "../utils/json";
 import { DboSensorData } from "../db/sensor-data";
 import { randomUUID } from "crypto";
 
@@ -16,13 +9,9 @@ const sqlOutput = output.sql({
 });
 
 export async function test(
-  request: HttpRequest,
+  myTimer: Timer,
   context: InvocationContext
-): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
-
-  const name = request.query.get("name") || (await request.text()) || "world";
-
+): Promise<void> {
   try {
     const data = await sensors.collectData();
     const sqlData: DboSensorData[] = data.map((d) => ({
@@ -35,22 +24,13 @@ export async function test(
       recorded_at: new Date(d.recordedAt),
     }));
     context.extraOutputs.set(sqlOutput, sqlData);
-
-    return json({
-      message: `Hello, ${name}!`,
-      data: sqlData,
-    });
   } catch (error) {
-    return json({
-      message: `Hello, ${name}!`,
-      error,
-    });
+    context.error(error);
   }
 }
 
-app.http("test", {
-  methods: ["POST"],
-  authLevel: "anonymous",
+app.timer("collect-sensor-data", {
+  schedule: "*/5 * * * *",
   extraOutputs: [sqlOutput],
   handler: test,
 });
